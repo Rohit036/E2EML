87fee5f7b143cc9a5d224910e3b0279bed413273
Rohit036 <kumarsingh.rohit7@gmail.com>
2023-03-10 20:06:51 +0530

    coment change

--- src/exception.py
+++ src/exception.py
@@ -18,7 +18,7 @@ class CustomException(Exception):
         return self.error_message
 
 # Use this to test the exception handling.
-#  if __name__ == "__main__":
+# if __name__ == "__main__":
 #     try:
 #         a = 1/0
 #     except Exception as e:
